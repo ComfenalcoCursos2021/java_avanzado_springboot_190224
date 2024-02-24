fc5276a7dcfe48e252b9defee482d94e595206fa
alvaro.obregon <alvaro911>
2024-02-23 19:02:13 -0500

    Clase 20240223 cree el clon de pagina usuario

--- WORKSPACE_ECLIPSE/ProyectoSpringEclipse/src/main/java/com/grupoi/base/controladores/UsuarioControlador.java
+++ WORKSPACE_ECLIPSE/ProyectoSpringEclipse/src/main/java/com/grupoi/base/controladores/UsuarioControlador.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UsuarioControlador {
 
-	@GetMapping("getdesdeeclipsepagina")
+	@GetMapping("paginausuario")
 	public String getMethodName() {
+		return "pagina";
+	}
+	@GetMapping("paginausuarioclon")
+	public String getMethodName2() {
 		return "pagina";
 	}
 	
